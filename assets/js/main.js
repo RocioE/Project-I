40ec60f653306bb8f67ce4462edcc9596d2d217d
koa2019 <no-reply>
2019-08-01 17:06:55 -0700

    added a .ready() to the begining of main.js & it fixed the loading issue. captured user input for artist & track. called updatePage() to show user input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,50 @@
-$.ajax({
-    type: "GET",
-    data: {
-        apikey: "65556a2efa1feefbbd18ccb3228569c4",
-        q_track: "back to december",
-        q_artist: "taylor%20swift",
-        f_has_lyrics: 1,
-        format: "jsonp",
-        callback: "jsonp_callback"
-    },
-    url: "http://api.musixmatch.com/ws/1.1/track.search",
-    dataType: "jsonp",
-    jsonpCallback: 'jsonp_callback',
-    contentType: 'application/json',
-    success: function(data) {
-        console.log(data);
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorThrown);
+$(document).ready(function() {
+
+    var artist;
+    var track;
+
+    $.ajax({
+        type: "GET",
+        data: {
+            apikey: "65556a2efa1feefbbd18ccb3228569c4",
+            q_track: "back to december",
+            q_artist: "taylor%20swift",
+            f_has_lyrics: 1,
+            format: "jsonp",
+            callback: "jsonp_callback"
+        },
+        url: "http://api.musixmatch.com/ws/1.1/track.search",
+        dataType: "jsonp",
+        jsonpCallback: 'jsonp_callback',
+        contentType: 'application/json',
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
+    // var q_artist = ['taylor%20swift', 'beyonce'];
+
+    $('#submit').on('click', function getArtist(event) {
+        event.preventDefault();
+
+        artist = $('#artist-input').val().trim();
+        track = $('#track-input').val().trim();
+
+        console.log(artist + '' + track);
+        //push artist to artist array
+        // q_artist.push(artist);
+
+        updatePage();
+    })
+
+    function updatePage() {
+        console.log('hit updatePage()');
+        $('.artist').text(artist);
+        $('.track').append(track);
     }
+
 });
-var q_artist = ['taylor%20swift', 'beyonce'];
-
-$('#submit').on('click', function getArtist(event) {
-    event.preventDefault();
-
-    artist = $('#artist-input').val().trim();
-    // track = $('#track-input’).val().trim();
-
-    //push artist to artist array
-    q_artist.push(artist);
-
-    updatePage();
-})
-
-function updatePage() {
-
-    $('.artist').text(artist);
-}
