176d85020a724d586c8c759ac3b4f2009ef7a425
koa2019 <no-reply>
2019-08-05 09:39:45 -0700

    changed videoId for player on profile.html to be taylor swift's back to december

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -23,7 +23,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '390',
         width: '640',
-        videoId: 'M7lc1UVf-VE',
+        videoId: 'QUwxKWT6m7U', //taylor swift - back to december
         events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
